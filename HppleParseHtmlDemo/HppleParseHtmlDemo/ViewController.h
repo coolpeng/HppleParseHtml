a6c39dc129fc74a716cbfc7980f097a0dc51475e
彭连进 <cool_plj@163.com>
2018-11-06 18:10:53 +0800

    finished project build

---
+++ HppleParseHtmlDemo/HppleParseHtmlDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HppleParseHtmlDemo
+//
+//  Created by Edward on 2018/11/6.
+//  Copyright © 2018年 coolpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HppleParseHtmlDemo/HppleParseHtmlDemoUITests/HppleParseHtmlDemoUITests.m
@@ -0,0 +1,38 @@
+//
+//  HppleParseHtmlDemoUITests.m
+//  HppleParseHtmlDemoUITests
+//
+//  Created by Edward on 2018/11/6.
+//  Copyright © 2018年 coolpeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HppleParseHtmlDemoUITests : XCTestCase
+
+@end
+
+@implementation HppleParseHtmlDemoUITests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
